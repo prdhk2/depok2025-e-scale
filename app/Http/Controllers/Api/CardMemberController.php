0563232aebe24a16e6eefeedd99a18c1db5fa282
prdhk2 <pradhika2112@gmail.com>
2025-05-21 15:03:58 +0700

    -u cardMember

--- app/Http/Controllers/Api/CardMemberController.php
+++ app/Http/Controllers/Api/CardMemberController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\CardMember;
 use Illuminate\Http\Request;
 
 class CardMemberController {
+
     public function CardRegister(Request $request) {
+        
         $request->validate([
-            'card_number'       => 'required|numeric'
+            'card_number'       => 'required|numeric',
+            'driver'            => 'required',
+            'no_pol'            => 'required',
+            'car_type'          => 'required'
         ]);
+
+        CardMember::create([
+            'card_member'   => $request->card_number,
+            'driver'        => $request->driver,
+            'no_pol'        => $request->no_pol,
+            'car_type'      => $request->car_type
+        ]);
+
+        return response()->json(['success' => true]);
     }
 }
